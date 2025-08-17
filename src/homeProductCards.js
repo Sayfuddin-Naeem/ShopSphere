93a71be6a3192545249cebffe02ba20a74f0ef76
Sayfuddin Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2025-08-17 18:01:54 +0600

    Show product on index.html using javascript from product.json file

---
+++ src/homeProductCards.js
@@ -0,0 +1,32 @@
+const productContainer = document.querySelector('#productContainer');
+const productTemplate = document.querySelector('#productTemplate');
+
+export const showProductContainer = (products) => {
+    if(!products){
+        return false;
+    }
+
+    products.forEach(curProduct => {
+        const {id, name, category, brand, price, stock, description, image} = curProduct;
+
+        const productClone = document.importNode(productTemplate.content, true);
+
+        productClone.querySelector('.category').textContent = category;
+        productClone.querySelector('.productImage').src = image;
+        productClone.querySelector('.productImage').alt = name;
+        productClone.querySelector('.productName').textContent = name;
+        productClone.querySelector('.productDescription').textContent = description;
+        productClone.querySelector('.productPrice').textContent = `\u09F3${price}`;
+        productClone.querySelector('.productActualPrice').textContent = `\u09F3${price * 4}`;
+
+        productClone.querySelector('.productStock').textContent = stock;
+
+        productClone
+        .querySelector('.stockElement')
+        .addEventListener('click', (ev) => {
+            homeQuantityToggle(ev, id, stock);
+        });
+
+        productContainer.append(productClone);
+    });
+};
